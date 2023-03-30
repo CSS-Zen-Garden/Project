d5a8cc759d53a02efd80f8b1fa33a9195815bd0c
0m0hsam <omohsam81@gmail.com>
2023-03-30 05:27:13 +0100

    new update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.clear();
 const sidebar = document.querySelector(".sidebar");
 const span = document.createElement("span");
+const footer = document.createElement("footer");
 sidebar.appendChild(span);
 sidebar.insertBefore(span, sidebar.children[0]);
 
